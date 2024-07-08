b7aced1ebe4c06584d4813d7245563d1d808a96e
Kerim Melikoglu <Kerim.Melikoglu@my-pv.com>
2024-07-08 10:33:57 +0200

    replaced async_forward_entry with async_forward_entries

--- custom_components/mypv/__init__.py
+++ custom_components/mypv/__init__.py
@@ -63,6 +63,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         DATA_COORDINATOR: coordinator,
     }
 
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
+
