3642a9992dc8942191c0892abc57ea38eae043b0
Kerim Melikoglu <Kerim.Melikoglu@my-pv.com>
2024-07-08 10:06:40 +0200

    Added await to async_forward_entry_setup

--- custom_components/mypv/__init__.py
+++ custom_components/mypv/__init__.py
@@ -64,6 +64,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     }
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
