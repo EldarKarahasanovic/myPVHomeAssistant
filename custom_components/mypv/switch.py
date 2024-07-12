0081125b39244605dac5320242d9d026e548b2ce
Kerim Melikoglu <Kerim.Melikoglu@my-pv.com>
2024-07-12 08:14:59 +0200

    The state of the switch is updated every 10 seconds

--- custom_components/mypv/switch.py
+++ custom_components/mypv/switch.py
@@ -3,7 +3,12 @@
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.const import CONF_HOST
+
+from .const import DOMAIN, DATA_COORDINATOR
+from .coordinator import MYPVDataUpdateCoordinator
+
 
 import logging
 import aiohttp
@@ -13,20 +18,23 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up the SwitchBoost switch."""
     host = entry.data[CONF_HOST]
-    async_add_entities([BoostSwitch(host)], True)
+    coordinator: MYPVDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
+        DATA_COORDINATOR
+    ]
+    async_add_entities([BoostSwitch(host, coordinator)], True)
     return True
 
-class BoostSwitch(SwitchEntity):
-    def __init__(self, host):
+class BoostSwitch(SwitchEntity, CoordinatorEntity):
+    def __init__(self, host, coordinator):
         """Initialize the switch"""
+        super().__init__(coordinator)
         self._name = "Toggle switch"
-        self._is_on = True
         self._host = host
     
     @property
     def is_on(self):
-        _LOGGER.warning("Zustand Switch wird abgefragt")
-        return self._is_on
+        deviceState = self.coordinator.data["setup"]["devmode"]
+        return deviceState == 1
 
     @property
     def name(self):
@@ -38,7 +46,7 @@ class BoostSwitch(SwitchEntity):
             async with session.get(f"http://{self._host}/data.jsn?devmode=1") as response:
                 if response.status != 200:
                     _LOGGER.error(f"Failed to turn on the device: {self._name}")
-
+        await self.coordinator.async_request_refresh()
         self.async_write_ha_state()
 
     
@@ -49,4 +57,6 @@ class BoostSwitch(SwitchEntity):
                 if response.status != 200:
                     _LOGGER.error(f"Failed to turn off the device: {self._name}")
 
+        await self.coordinator.async_request_refresh()
         self.async_write_ha_state()
+
