654513bd750690cb7456fe414f07a59a0d582b49
Kerim Melikoglu <Kerim.Melikoglu@my-pv.com>
2024-07-15 12:08:13 +0200

    Removed coordinator in switch.py

--- custom_components/mypv/switch.py
+++ custom_components/mypv/switch.py
@@ -1,7 +1,6 @@
 """Switch entity"""
 
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST
@@ -9,41 +8,35 @@ from homeassistant.const import CONF_HOST
 import logging
 import aiohttp
 
-from .const import DOMAIN, DATA_COORDINATOR
-from .coordinator import MYPVDataUpdateCoordinator
-
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up the toggle switch."""
-    coordinator: MYPVDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
-        DATA_COORDINATOR
-    ]
-
     host = entry.data[CONF_HOST]
-    async_add_entities([ToggleSwitch(coordinator, host)], True)
+    async_add_entities([ToggleSwitch(host)], True)
 
-class ToggleSwitch(CoordinatorEntity, SwitchEntity):
-    def __init__(self, coordinator, host):
+class ToggleSwitch(SwitchEntity):
+    def __init__(self, host):
         """Initialize the switch"""
-        super().__init__(coordinator)
-        self.coordinator = coordinator
         self._name = "Device state"
         self._host = host
+        self._is_on = False
     
     @property
     def is_on(self):
-        return self.coordinator.data["setup"].get("devmode", False)
+        return self._is_on
 
     @property
     def name(self):
         return self._name
     
     async def async_turn_on(self):
+        self._is_on = True
         await self.async_toggle_switch(1)
         self.async_write_ha_state()
 
     async def async_turn_off(self):
+        self._is_on = False
         await self.async_toggle_switch(0)
         self.async_write_ha_state()
     
@@ -52,5 +45,3 @@ class ToggleSwitch(CoordinatorEntity, SwitchEntity):
             async with session.get(f"http://{self._host}/data.jsn?devmode={mode}") as response:
                 if response.status != 200:
                     _LOGGER.error(f"Failed to turn on/off the device {self._entity_id}")
-                else:
-                    await self.coordinator.async_request_refresh()
