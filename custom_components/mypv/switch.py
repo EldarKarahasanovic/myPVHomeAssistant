a63ea7b92fff5e2c3a4eaa8b175ea64f6bc3ad3f
Kerim <kerimmelikoglu@gmail.com>
2024-07-22 07:29:07 +0200

    Updated switch.py

--- custom_components/mypv/switch.py
+++ custom_components/mypv/switch.py
@@ -3,7 +3,11 @@
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.const import CONF_HOST
+
+from .const import DOMAIN, DATA_COORDINATOR
+from .coordinator import MYPVDataUpdateCoordinator
 
 import logging
 import aiohttp
@@ -12,15 +16,20 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up the toggle switch."""
+    coordinator: MYPVDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR]
     host = entry.data[CONF_HOST]
-    async_add_entities([ToggleSwitch(host)], True)
+    async_add_entities([ToggleSwitch(coordinator, host)], True)
 
-class ToggleSwitch(SwitchEntity):
-    def __init__(self, host):
+class ToggleSwitch(CoordinatorEntity, SwitchEntity):
+    def __init__(self, coordinator, host):
         """Initialize the switch"""
-        self._name = "Device state"
+        super().__init__(coordinator)
+        self.coordinator = coordinator
+        self._name = f"Device state {host}"
         self._host = host
         self._is_on = False
+        self._model = self.coordinator.data["info"]["device"]
+        self.serial_number = self.coordinator.data["info"]["sn"]
     
     @property
     def is_on(self):
@@ -45,3 +54,13 @@ class ToggleSwitch(SwitchEntity):
             async with session.get(f"http://{self._host}/data.jsn?devmode={mode}") as response:
                 if response.status != 200:
                     _LOGGER.error(f"Failed to turn on/off the device {self._entity_id}")
+    
+    @property
+    def device_info(self):
+        """Return information about the device."""
+        return {
+            "identifiers": {(DOMAIN, self.serial_number)},
+            "name": self._name,
+            "manufacturer": "my-PV",
+            "model": self._model,
+        }
