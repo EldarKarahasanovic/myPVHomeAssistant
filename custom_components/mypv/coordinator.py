ae6e0c0b71663d2c82cb7d057a897125a761d735
Kerim <kerimmelikoglu@gmail.com>
2024-07-24 14:09:24 +0200

    Error handling in coordinator.py

--- custom_components/mypv/coordinator.py
+++ custom_components/mypv/coordinator.py
@@ -1,4 +1,4 @@
-"""Provides the MYPV DataUpdateCoordinator."""
+"""Provides the my-PV DataUpdateCoordinator."""
 from datetime import timedelta
 import logging
 import aiohttp
@@ -15,14 +15,13 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class MYPVDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching MYPV data."""
+    """Class to manage fetching my-PV data."""
 
     def __init__(self, hass: HomeAssistant, *, config: dict):
-        """Initialize global NZBGet data updater."""
+        """Initialize my-PV coordinator."""
         self._host = config[CONF_HOST]
         self._info = None
         self._setup = None
-        self._next_update = 0
         self._data = "data.jsn"
         update_interval = timedelta(seconds=10)
 
@@ -34,29 +33,32 @@ class MYPVDataUpdateCoordinator(DataUpdateCoordinator):
         )
 
     async def _async_update_data(self) -> dict:
-        """Fetch data"""
+        """Fetch data."""
         try:
             async with aiohttp.ClientSession() as session:
                 if self._info is None:
                     self._info = await self.async_update_info(session)
                     if self._info is None:
                         raise Exception("Could not connect to your my-PV device")
+                
                 if self._data != "monitorjson":
                     self._setup = await self.async_update_setup(session)
                     if self._setup is None:
                         raise Exception("Could not connect to your my-PV device")
+                
                 data = await self.async_update_data(session)
                 if data is None:
                     raise Exception("Could not connect to your my-PV device")
                 
                 return {
-                    "data" : data,
-                    "info" : self._info,
-                    "setup" : self._setup,
+                    "data": data,
+                    "info": self._info,
+                    "setup": self._setup,
                 }
         except Exception as e:
             _LOGGER.error(f"Error fetching data from the API: {e}")
-        
+            return {}
+
     async def async_update_info(self, session):
         try:
             timeout = ClientTimeout(total=5)
@@ -67,11 +69,13 @@ class MYPVDataUpdateCoordinator(DataUpdateCoordinator):
                         self._data = "monitorjson"
                     return info
                 else:
-                    _LOGGER.error("Failed to connect to your my-PV device")
+                    _LOGGER.error(f"Failed to connect to your my-PV device (status code: {response.status})")
                     return None
-        except aiohttp.ClientError:
+        except aiohttp.ClientError as e:
+            _LOGGER.error(f"ClientError in async_update_info: {e}")
             return None
-        except asyncio.TimeoutError:
+        except asyncio.TimeoutError as e:
+            _LOGGER.error(f"TimeoutError in async_update_info: {e}")
             return None
     
     async def async_update_setup(self, session):
@@ -82,11 +86,13 @@ class MYPVDataUpdateCoordinator(DataUpdateCoordinator):
                     setup = await response.json()
                     return setup
                 else:
-                    _LOGGER.error("Failed to connect to your my-PV device")
+                    _LOGGER.error(f"Failed to connect to your my-PV device (status code: {response.status})")
                     return None
-        except aiohttp.ClientError:
+        except aiohttp.ClientError as e:
+            _LOGGER.error(f"ClientError in async_update_setup: {e}")
             return None
-        except asyncio.TimeoutError:
+        except asyncio.TimeoutError as e:
+            _LOGGER.error(f"TimeoutError in async_update_setup: {e}")
             return None
         
     async def async_update_data(self, session):
@@ -97,9 +103,11 @@ class MYPVDataUpdateCoordinator(DataUpdateCoordinator):
                     data = await response.json()
                     return data
                 else:
-                    _LOGGER.error("Failed to connect to your my-PV device")
+                    _LOGGER.error(f"Failed to connect to your my-PV device (status code: {response.status})")
                     return None
-        except aiohttp.ClientError:
+        except aiohttp.ClientError as e:
+            _LOGGER.error(f"ClientError in async_update_data: {e}")
             return None
-        except asyncio.TimeoutError:
+        except asyncio.TimeoutError as e:
+            _LOGGER.error(f"TimeoutError in async_update_data: {e}")
             return None
